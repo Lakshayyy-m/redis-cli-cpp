a1982f67a0123f53d182ad0edbd96782275755f6
Lakshay Manchanda <lakshaymanchanda06@gmail.com>
2026-05-17 23:17:02 -0700

    Initial client connection and disconnection established

--- CLI.cpp
+++
@@ -1,4 +0,0 @@
-#include "CLI.h"
-#include <string>
-
-CLI::CLI(const std::string &host, int port) : redisClient(host, port) {} 
--- RedisClient.cpp
+++
@@ -1,8 +0,0 @@
-#include "RedisClient.h"
-#include <string>
-
-RedisClient::RedisClient(const std::string &host, int port){}
-
-bool RedisClient::connectToServer(){
-
-}
